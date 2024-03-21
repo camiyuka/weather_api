c959fdd9490f3f9313b6d5abf8c15a19789a1376
camilayatabe <camila.yatabe@gmail.com>
2024-03-20 22:39:49 -0300

    fix: fix serializer

--- weather/serializers.py
+++ weather/serializers.py
@@ -1,24 +1,10 @@
 from rest_framework import serializers
 from .models import WeatherEntity
-from rest_framework import serializers
 
 class WeatherSerializer(serializers.Serializer):
-    temperature = serializers.DecimalField(max_digits=5, decimal_places=2)
-    date = serializers.DateField()
+    temperature = serializers.FloatField()
+    # date = serializers.DateField(required=False)  
     city = serializers.CharField(max_length=100, required=False)
-    atmosphericPressure = serializers.CharField(max_length=100, required=False)
-    humidity = serializers.CharField(max_length=100, required=False)
+    atmospheric_pressure = serializers.FloatField( required=False)
+    humidity = serializers.FloatField( required=False)
     weather = serializers.CharField(max_length=100, required=False)
-
-    def create(self, validated_data):
-        return WeatherEntity(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.temperature = validated_data.get('temperature', instance.temperature)
-        instance.date = validated_data.get('date', instance.date)
-        instance.city = validated_data.get('city', instance.city)
-        instance.atmosphericPressure = validated_data.get('atmosphericPressure', instance.atmosphericPressure)
-        instance.humidity = validated_data.get('humidity', instance.humidity)
-        instance.weather = validated_data.get('weather', instance.weather)
-        return instance
-
