1c2cd5100582f54cefc3e9d72d37c818f89da527
camilayatabe <camila.yatabe@gmail.com>
2024-03-21 21:58:39 -0300

    chore: implementing treatment for the date field

--- weather/models.py
+++ weather/models.py
@@ -14,3 +14,9 @@ class WeatherEntity:
 
     def __str__(self) -> str:
         return (f"Weather <{self.temperature}>")
+    
+    def __getattribute__(self, __name: str) -> any:
+        if (__name=='date'):
+            return object.__getattribute__(self, __name).strftime("%d/%m/%Y %H:%M:%S")
+        else:
+            return object.__getattribute__(self, __name)
