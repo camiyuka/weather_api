eb362bc59cf216ba6f9e953d42cdccaa3a92a26c
camilayatabe <camila.yatabe@gmail.com>
2024-03-21 21:58:53 -0300

    chore: date serializer

--- weather/serializers.py
+++ weather/serializers.py
@@ -3,8 +3,12 @@ from .models import WeatherEntity
 
 class WeatherSerializer(serializers.Serializer):
     temperature = serializers.FloatField()
-    # date = serializers.DateField(required=False)  
+    date = serializers.DateField(required=False)  
     city = serializers.CharField(max_length=100, required=False)
     atmospheric_pressure = serializers.FloatField( required=False)
     humidity = serializers.FloatField( required=False)
     weather = serializers.CharField(max_length=100, required=False)
+
+           
+        
+    
